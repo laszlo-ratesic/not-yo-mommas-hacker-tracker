01baf507b61a1ff85bb41e58f87faa212590faae
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-03-14 16:57:15 -0500

    move title card

--- db/connection.js
+++ db/connection.js
@@ -1,29 +1,16 @@
-const mysql = require('mysql2');
-const gradient = require('gradient-string');
+const mysql = require("mysql2");
+const gradient = require("gradient-string");
 
 // Connect to the database
 const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: 'hackers_db'
-    },
-    console.clear(),
-    console.log(
-        gradient('silver', 'silver', 'red', 'red', 'gold')(`
-        ██▀███  ▓█████ ▄▄▄    ██▒   █▓▓█████  ██▀███      ▄████▄   ███▄ ▄███▓  ██████
-        ▓██ ▒ ██▒▓█   ▀▒████▄ ▓██░   █▒▓█   ▀ ▓██ ▒ ██▒   ▒██▀ ▀█  ▓██▒▀█▀ ██▒▒██    ▒
-        ▓██ ░▄█ ▒▒███  ▒██  ▀█▄▓██  █▒░▒███   ▓██ ░▄█ ▒   ▒▓█    ▄ ▓██    ▓██░░ ▓██▄
-        ▒██▀▀█▄  ▒▓█  ▄░██▄▄▄▄██▒██ █░░▒▓█  ▄ ▒██▀▀█▄     ▒▓▓▄ ▄██▒▒██    ▒██   ▒   ██▒
-        ░██▓ ▒██▒░▒████▒▓█   ▓██▒▒▀█░  ░▒████▒░██▓ ▒██▒   ▒ ▓███▀ ░▒██▒   ░██▒▒██████▒▒
-        ░ ▒▓ ░▒▓░░░ ▒░ ░▒▒   ▓▒█░░ ▐░  ░░ ▒░ ░░ ▒▓ ░▒▓░   ░ ░▒ ▒  ░░ ▒░   ░  ░▒ ▒▓▒ ▒ ░
-          ░▒ ░ ▒░ ░ ░  ░ ▒   ▒▒ ░░ ░░   ░ ░  ░  ░▒ ░ ▒░     ░  ▒   ░  ░      ░░ ░▒  ░ ░
-          ░░   ░    ░    ░   ▒     ░░     ░     ░░   ░    ░        ░      ░   ░  ░  ░
-           ░        ░  ░     ░  ░   ░     ░  ░   ░        ░ ░             ░         ░
-                                   ░                      ░
-        Connected to hacker_db.`)
-    )
+  {
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "hackers_db",
+  },
+  console.clear(),
+  console.log("Connection success.")
 );
 
 module.exports = db;

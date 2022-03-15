bd2b02b33d3193a37ce1d24ff2e0572a8b9880b8
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-03-15 12:32:23 -0500

    fix add employee control flow bugs

--- db/schema.sql
+++ db/schema.sql
@@ -12,8 +12,8 @@ CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES departments(id)
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
